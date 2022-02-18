39c094522bd71be762f57fcec1cb7882153842bf
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-02-18 13:25:23 -0800

    adds axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
-const App = () => {
+function App() {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+      .then(res => {
+        setUsers(res.data);
+      }).catch(err => console.error(err))
+  })
   return (
     <div className="App">
       <h1 className="Header">Welcome to Jar Jar Book</h1>
